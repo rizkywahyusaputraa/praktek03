bc80cb5961aa10f3aeffd96c12586de66f706a6a
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-04 17:04:54 +0800

    membuat atribut dan method

--- src/praktek03/anaksidin.java
+++ src/praktek03/anaksidin.java
@@ -1,6 +1,11 @@
 
 package praktek03;
 
-public class anaksidin extends {
+public class anaksidin extends bapak {
     
+    private String nama_anak1;
+    
+    void throttle (){
+        System.out.println("Siti Fatimahtul Zahra");
+    }
 }
