491862866778442f1633b6692cf2bfafef633da7
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-04 17:06:58 +0800

    geteer and stter

--- src/praktek03/anaksidin.java
+++ src/praktek03/anaksidin.java
@@ -3,9 +3,19 @@ package praktek03;
 
 public class anaksidin extends bapak {
     
-    private String nama_anak1;
+    private String nama_anak;
     
     void throttle (){
         System.out.println("Siti Fatimahtul Zahra");
     }
+
+    public String getNama_anak() {
+        return nama_anak;
+    }
+
+    public void setNama_anak(String nama_anak) {
+        this.nama_anak = nama_anak;
+    }
+    
+    
 }
