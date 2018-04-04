bfe75febef3f4430e4e6a02bf4c653929ba4556a
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-04 16:33:28 +0800

    Membuat method cetak info ()

--- src/praktek03/bapak.java
+++ src/praktek03/bapak.java
@@ -4,6 +4,11 @@ package praktek03;
 public class bapak {
     String nama;
     String hobi;
-    String Makananfavorit;
+    String makananfavorit;
     
+    void cetakinfo(){
+            System.out.println("nama \t: "+nama+"\n"+
+                    "hobi \t: "+hobi+"\n"+
+                    "makananfavorit \t:"+makananfavorit);
+    }
 }
