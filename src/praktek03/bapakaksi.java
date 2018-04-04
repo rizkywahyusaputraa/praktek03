8a1b9a4de52d92f81a9bc51b34da15f8a8186da9
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-04 16:42:09 +0800

    Instance Membuat atribut dari pannggil Method

--- src/praktek03/bapakaksi.java
+++ src/praktek03/bapakaksi.java
@@ -2,6 +2,13 @@
 package praktek03;
 
 public class bapakaksi {
-    
+    public static void main(String[] args) {
+        bapak b = new bapak();
+        b.nama ="Rizky wahyu saputra";
+        b.hobi ="Membaca Koran";
+        b.makananfavorit ="Bakso+nasi";
+        
+        b.cetakinfo();
+    }
     
 }
